6b545c7f21ebeb4bb7ef9c74f1cb3acdd8c52575
BrettM265 <BDMgit@gmail.com>
2023-06-28 20:43:28 -0500

    Updates

---
+++ precache-manifest.ac610702f771d1624d69b8284f7c4626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e02a0eeb0c70f80f675",
+    "url": "/portfolio2023/static/css/main.3481f5f2.chunk.css"
+  },
+  {
+    "revision": "7e02a0eeb0c70f80f675",
+    "url": "/portfolio2023/static/js/main.7e02a0ee.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/portfolio2023/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "2cc285526b2889b040da",
+    "url": "/portfolio2023/static/js/2.2cc28552.chunk.js"
+  },
+  {
+    "revision": "a1f0407b78f020d580ca",
+    "url": "/portfolio2023/static/js/runtime~main.a1f0407b.js"
+  },
+  {
+    "revision": "1e980f9bfbf479bff162ff1c77e11a44",
+    "url": "/portfolio2023/index.html"
+  }
+];
