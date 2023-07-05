3a81dbdee4e600de274c54c9531aedd617038eee
BrettM265 <BDMgit@gmail.com>
2023-07-05 18:01:59 -0500

    commit 7-5-23

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Header />
       <div className='Body'>
       <Experience />
-      {/*<SkillsSection />*/}
+      <SkillsSection />
       <ProjectSection />
       <Footer />
       </div>
